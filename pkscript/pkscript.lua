bac15f62a5cf0d777169eb8e63aa94bfcad2477b
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2025-04-12 18:52:47 -0400

    Add fullunload command

--- pkscript/pkscript.lua
+++ pkscript/pkscript.lua
@@ -18,4 +18,19 @@ function pkscript.Unload()
 	pkscript.Hooks.UnRegisterAll()
 end
 
+function pkscript.FullUnload()
+	pkscript.Unload()
+
+	local CommandTable = concommand.GetTable()
+
+	for Command, _ in next, CommandTable do
+		if string.StartsWith(Command, "pkscript_") then
+			concommand.Remove(Command)
+		end
+	end
+
+	pkscript = nil
+end
+
 concommand.Add("pkscript_unload", pkscript.Unload)
+concommand.Add("pkscript_fullunload", pkscript.FullUnload)
