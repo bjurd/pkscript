ee28b26df32f5713960615574364469a7fe7d617
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2025-04-16 20:41:28 -0400

    Make cleanup on release only remove if it'll hit you

--- pkscript/miscellaneous.lua
+++ pkscript/miscellaneous.lua
@@ -7,7 +7,7 @@ local Config = Misc.Config
 Config.AutoCleanup = Config.AutoCleanup or {}
 Config.AutoCleanup.Enabled = pkscript.Util.ConfigDefault(Config.AutoCleanup.Enabled, false)
 Config.AutoCleanup.OnDeath = pkscript.Util.ConfigDefault(Config.AutoCleanup.OnDeath, false)
-Config.AutoCleanup.OnRelease = pkscript.Util.ConfigDefault(Config.AutoCleanup.OnRelease, false) -- This isn't useful for propkill since it'll just disappear when you throw, useful for just flyin' around
+Config.AutoCleanup.OnRelease = pkscript.Util.ConfigDefault(Config.AutoCleanup.OnRelease, false)
 
 do -- Auto Cleanup
 	local AliveLast
@@ -30,6 +30,19 @@ do -- Auto Cleanup
 		end
 
 		if Player == pkscript.LocalPlayer then -- TODO: Only remove the prop released? I don't this is possible :(
+			local ToPlayer = pkscript.LocalPlayer:GetPos()
+			ToPlayer:Sub(Entity:GetPos())
+			ToPlayer:Normalize()
+
+			local Direction = Entity:GetVelocity()
+			Direction:Normalize()
+
+			local Dot = Direction:Dot(ToPlayer)
+
+			if Dot <= math.cos(30) then -- 30 fov seems good enough /shrug
+				return -- Only remove if it's heading towards us
+			end
+
 			pkscript.LocalPlayer:ConCommand("gmod_cleanup")
 		end
 	end
